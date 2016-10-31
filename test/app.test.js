5a398acb3e2cf8f43a19940232b2f2b698264e7f
Gloria Guy <totallygloria@gmail.com>
2016-10-31 13:48:11 -0700

    Added the city files for testing purposes, and copied over the single-resource tests. Changed some of the file names, still need to move from query strings to routes, only the server 200 OK test is passing atm, as most of the API hasn't been written yet.

---
+++ test/app.test.js
@@ -0,0 +1,173 @@
+const chai = require('chai');
+const assert = chai.assert;
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+const path = require('path');
+const rimraf = require('rimraf');
+
+const ds = require('../lib/datastore');
+const app = require('../lib/app');
+
+
+describe('Express http server API', () => {
+
+  const testDir = path.join(__dirname, 'test-dir');
+  const server = chai.request(app);
+
+  // before(done => {
+  //   // if the portland file is still around, remove it. ditch the error if it's not around.
+  //   sander.unlink('/home/driel/projects/CodeFellows401/lab_assignments/class08-http-single-resource/city/portland.json')
+  //     .then(done)
+  //     .catch((err) => {
+  //       done();
+  //     });
+  // });
+
+
+  it('hits the root and gets a 200 Ok', done => {
+    server
+      .get('/')
+      .end((err, res)=> {
+        if (err) return done(err);
+        assert.equal(res.status, 200);
+        done();
+      });
+  });
+
+  it('retrieves all files in the city folder by name', done => {
+
+    const testHtml = '<h2>The data store contains the following files: </h2><ul>\n\n' +
+      '<li>boston.json</li>' +
+      '<li>los_angeles.json</li>' +
+      '<li>miami.json</li>' +
+      '<li>new_york.json</li>' +
+      '<li>san_francisco.json</li>' +
+      '</ul>' +
+      'To retrieve a file, please query the file name';
+
+    server
+      .get('/city')
+      .end((err, res) => {
+        if (err) return done(err);
+        assert.equal(res.text, testHtml);
+        done();
+      })
+  });
+
+  it('retrieves the contents of a file', done => {
+
+    const expectedResults = '{"City":"Boston","State":"MA","Mean_1_Bdrm_Price":"$2025"}\n';
+
+    server
+      .get('/city?id=boston.json')
+      .end((err, res) => {
+        if (err) return done(err);
+        assert.equal(res.text, expectedResults);
+        done();
+      })
+  });
+
+
+  it('writes post data to a file', done => {
+
+    const expectedResults = 'File san_diego.json saved.';
+
+    server
+      .post('/city')
+      .send({"City":"San Diego","State":"CA","Median_1_BR_price":"$1,500","Median_2_BR_price":"$2,100"})
+      .end((err, res) => {
+        if (err) return done(err);
+        assert.equal(res.text, expectedResults);
+        done();
+      })
+  });
+
+  it('retrieves all files in the city folder by name', done => {
+
+    const testHtml = '<h2>The data store contains the following files: </h2><ul>\n\n' +
+      '<li>boston.json</li>' +
+      '<li>los_angeles.json</li>' +
+      '<li>miami.json</li>' +
+      '<li>new_york.json</li>' +
+      '<li>san_diego.json</li>' +
+      '<li>san_francisco.json</li>' +
+      '</ul>' +
+      'To retrieve a file, please query the file name';
+
+    server
+      .get('/city')
+      .end((err, res) => {
+        if (err) return done(err);
+        assert.equal(res.text, testHtml);
+        done();
+      })
+  });
+
+  it('retrieves the contents of a file added using POST method', done => {
+
+    const expectedResults = '{"City":"San Diego","State":"CA","Median_1_BR_price":"$1,500","Median_2_BR_price":"$2,100"}';
+
+    server
+      .get('/city?id=san_diego.json')
+      .end((err, res) => {
+        if (err) return done(err);
+        assert.equal(res.text, expectedResults);
+        done();
+      })
+  });
+
+  it('deletes the san_diego.json file', done => {
+
+    const expectedResults = 'san_diego.json deleted successfully.';
+
+    server
+      .del('/city?id=san_diego')
+      .end((err, res) => {
+        if (err) return done(err);
+        assert.equal(res.text, expectedResults);
+        done();
+      })
+  });
+
+
+  xit('writes a file using the PUT method', done => {
+
+    const expectedResults = 'File portland.json saved.';
+
+    server
+      .put('/city/portland.json')
+      .set('application', 'json')
+      .send({"City":"Portland","State":"OR","Median_1_BR_price":"$1,000","Median_2_BR_price":"$2,000"})
+      .end((err, res) => {
+        if (err) return done(err);
+        console.log('in the callback, err res', err, res);
+        assert.equal(res.text, expectedResults);
+        done();
+      })
+  });
+
+  xit('retrieves all files in the city folder by name', done => {
+
+    const testHtml = '<h2>The data store contains the following files: </h2><ul>\n\n' +
+      '<li>boston.json</li>' +
+      '<li>los_angeles.json</li>' +
+      '<li>miami.json</li>' +
+      '<li>new_york.json</li>' +
+      '<li>portland.json</li>' +
+      '<li>san_francisco.json</li>' +
+      '</ul>' +
+      'To retrieve a file, please query the file name';
+
+    server
+      .get('/city')
+      .end((err, res) => {
+        if (err) return done(err);
+        assert.equal(res.text, testHtml);
+        done();
+      })
+  });
+
+
+});
+
