b9f0ef114fda4c72364aa615dd9c4c442eb8398a
gnkatchmar <gunk55@msn.com>
2016-10-31 14:42:48 -0700

    1031-1440 stuick on Git

--- run-http-server.js
+++ run-http-server.js
@@ -1,4 +1,4 @@
-const server = require('./lib/http-server');
-const port = 8000;
+// const server = require('./lib/http-server');
+// const port = 8000;
 
-server.listen(8000);
+// server.listen(8000);
