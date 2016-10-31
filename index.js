f9bb42c7a60ebd4420f1247f0db5786859f0e022
DrewStock <andrewdstock@gmail.com>
2016-10-31 14:24:02 -0700

    using Express for GET, POST, PUT and DELETE; renamed index.js to server.js and taco_server.js to taco_express.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const server = require('./lib/taco_server');
-
-const port = 5000;
-
-server.listen(port, err => {
-    if(err) console.log('ERROR!', err);
-    else console.log('http server listening on port', port);
-});
