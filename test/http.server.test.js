e91c120964213ebf8ad99af23a5e40110ee2a608
Nathan Pickard <nathan.pickard87@gmail.com>
2016-11-01 16:22:59 -0700

    Adding project files from previous single resource lab

---
+++ test/http.server.test.js
@@ -0,0 +1,74 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+const expect = require('chai').expect;
+const assert = require('chai').assert;
+const server = require('../lib/football-server');
+const port = 8080;
+
+describe('server', () => {
+    before(done => {
+        server.listen(port, done);
+    });
+});
+
+describe('test http server resource', () => {
+    let request = chai.request(server);
+
+    it('sends back a response text', done => {
+        request
+            .get('/teams')
+            .end((err, response) => {
+                if(err) return done(err);
+                assert.deepEqual(response.text, 'broncos.json ' + 'seahawks.json ');
+                expect(response).to.have.status(200);
+                done();
+            });
+    });
+
+    it('request for resource in /teams', done => {
+        request
+        //   .get('/teams/seahawks')
+          .get('/teams/broncos')
+          .end((err, response) => {
+              if(err) return done(err);
+              assert.deepEqual(response.text, '{"city":"denver","conference":"afc"}');
+              expect(response).to.have.status(200);
+              done();
+          });
+    });
+
+        it('POST request to /teams/bears', done => {
+        request
+          .post('/teams/bears')
+          .send({"city":"chicago","conference":"nfc"})
+          .end((err, response) => {
+              if(err) return done(err);
+              assert.deepEqual(response.text, 'data has been written');
+              expect(response).to.have.status(200);
+              done();
+          });
+    });
+
+    it('PUT request to teams/bears', done => {
+        request
+          .put('/teams/bears')
+          .send({"division":"north"})
+          .end((err, response) => {
+              if(err) return done(err);
+              assert.deepEqual(response.text, 'data has been updated');
+              expect(response).to.have.status(200);
+              done();
+          });
+    });
+
+    it('DELETE request to teams/bears, deletes the resource', done => {
+        request
+          .delete('/teams/bears')
+          .end((err, response) => {
+              if(err) return done(err);
+              assert.deepEqual(response.text, 'file has been removed');
+              done();
+          });
+    });
+});
