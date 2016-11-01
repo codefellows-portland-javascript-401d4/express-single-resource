656a49d938af00bae96b0c1f95f870241ec306e5
DrewStock <andrewdstock@gmail.com>
2016-11-01 14:08:19 -0700

    added body-parser and test

---
+++ test/body-parser.test.js
@@ -0,0 +1,22 @@
+const bodyParser = require( '../lib/body-parser' )();
+const assert = require( 'chai' ).assert;
+const EventEmitter = require('events');
+
+describe('body parser middleware', () => {
+
+    it('parses body', done => {
+
+        const req = new EventEmitter();
+
+        const next = () => {
+            assert.deepEqual(req.body, { name: "here is some data" });
+            done();
+        };
+
+        bodyParser(req, null, next);
+
+        req.emit('data', '{ "name": "here is some data" }');
+        req.emit('end');
+
+    });
+});
