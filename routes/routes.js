b9f4c0cdbd690910640fdbc4f772c3bd3ae8144b
Albert Reel <waxhoya@gmail.com>
2016-11-01 13:32:34 -0700

    moving routes into its own doc

--- routes/routes.js
+++ routes/routes.js
@@ -17,25 +17,21 @@ router
         .catch(err => console.log(err));
     })
 
-    .get('/:id', (req, res) => {
-        store.get(req.params.id)
-        .then(pubData => res.send(pubData))
-        .catch(err => console.log(err));
-    })
-
     .delete('/:id', (req, res) => {
         store.remove(req.params.id)
             .then(result => res.send({ results: result }))
             .catch(err => console.log(err));
     })
 
-
-app.post('/team', (req, res) => {
-    let body = '';
-    req.on('data', data => body += data);
-    req.on('end', () => {
-        store.save(JSON.parse(body))
-            .then(id => res.send({ id: id }))
-            .catch(err => console.log(err))
+    .post('/team', (req, res) => {
+        let body = '';
+        req.on('data', data => body += data);
+        req.on('end', () => {
+            store.save(JSON.parse(body))
+                .then(id => res.send({ id: id }))
+                .catch(err => console.log(err))
+        })
     });
-});
+
+
+module.export = router;
