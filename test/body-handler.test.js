54b1606719911433780e00e0809083612d137548
Gloria Guy <totallygloria@gmail.com>
2016-11-01 20:16:27 -0700

    Added a second test to see if multiple data events were getting emitted and accumulated. Fairly artificial, but it does work.

--- test/body-handler.test.js
+++ test/body-handler.test.js
@@ -9,8 +9,7 @@ const bodyParser = require('../lib/body-handler');
 
 describe('Express server - bodyParsing middleware', () => {
 
-
-  it('accumulates and parses the body of dummy request', done => {
+  it('accumulates and parses the body of a single dummy request', done => {
 
     // create the dummy emitter and give it an id
     var req = new EventEmitter();
@@ -29,5 +28,28 @@ describe('Express server - bodyParsing middleware', () => {
     req.emit('end');
 
   });
+
+  it('accumulates and parses the body of a multiple dummy requests', done => {
+
+    // create the dummy emitter and give it an id
+    var req = new EventEmitter();
+    req.params = {id: 'portland.json'};
+
+    function next() {
+      assert.equal(req.body, '{"City":"Portland","State":"OR","Median_1_BR_price":"$1,000","Median_2_BR_price":"$2,000"}');
+      done();
+    }
+
+    // call the handler so it subscribes to events
+    bodyParser(req, null, next);
+    // emit data to check in the test
+    req.emit('data', '{"City":"Portland",' );
+    req.emit('data', '"State":"OR",' );
+    req.emit('data', '"Median_1_BR_price":"$1,000",' );
+    req.emit('data', '"Median_2_BR_price":"$2,000"}' );
+    req.emit('end');
+
+  });
+
 
 });
